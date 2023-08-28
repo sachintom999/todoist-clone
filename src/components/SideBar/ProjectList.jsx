b06bf31fde5444002350f9f0e8a0183118f252cb
Sachin Tom <sachintomacc@gmail.com>
2023-08-28 20:57:40 +0530

    Add new project

--- src/components/SideBar/ProjectList.jsx
+++ src/components/SideBar/ProjectList.jsx
@@ -2,6 +2,7 @@ import { useEffect } from "react"
 import { useDispatch, useSelector } from "react-redux"
 import { fetchAllProjects } from "../../redux/tasks"
 import SidebarItem from "./SidebarItem"
+import getIcon from "../../utils/getIcons"
 const ProjectList = () => {
     const dispatch = useDispatch()
 
@@ -12,8 +13,17 @@ const ProjectList = () => {
     }, [])
 
     return (
-        <div >
-            <p className="textfont-bold text-sm my-4">Projects</p>
+        <div>
+            <p className="textfont-bold text-sm my-4">
+                Projects
+                {getIcon("plus", {
+                    fontSize: 16,
+                    className: "ml-6 cursor-pointer hover:bg-slate-700",
+                    onClick: () => {
+                        console.log("open new project Modal.. PENDING")
+                    },
+                })}
+            </p>
             {projects?.map(project => (
                 <SidebarItem
                     key={project._id}
