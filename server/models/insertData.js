1a350e7eff0e8eb09115c728b3238103c9dff0f9
Sachin Tom <sachintomacc@gmail.com>
2023-09-01 10:10:23 +0530

    updated insert scripts

--- server/models/insertData.js
+++ server/models/insertData.js
@@ -1,18 +1,15 @@
-const mongoose = require("mongoose")
-const User = require("./user")
-const Label = require("./label")
-const Section = require("./section")
-const Task = require("./task")
-const Project = require("./project")
-const Comment = require("./comment")
+const mongoose = require("mongoose");
+const User = require("./user");
+const Label = require("./label");
+const Section = require("./section");
+const Task = require("./task");
+const Project = require("./project");
+const Comment = require("./comment");
 
-// const faker = require("faker")
+const path = require("path");require("dotenv").config({ path: path.resolve(__dirname, "../.env") })
 
-const path = require("path")
+mongoose.connect(process.env.MONGO_URI);
 
-require("dotenv").config({ path: path.resolve(__dirname, "../.env") })
-
-mongoose.connect(process.env.MONGO_URI)
 
 async function deleteData() {
     try {
@@ -28,180 +25,128 @@ async function deleteData() {
     }
 }
 
+
 async function insertSampleData() {
     try {
-        // Insert User
+        // Insert Users
         const user = await User.create({
             email: "john@example.com",
             firstName: "John",
             lastName: "Doe",
-            image: "https://e1.pxfuel.com/desktop-wallpaper/928/355/desktop-wallpaper-sm-on-twitter-random-people.jpg",
-        })
-        // const user2 = await User.create({
-        //     email: "sam@example.com",
-        //     firstName: "Sam",
-        //     lastName: "Matt",
-        //     image: "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQ6M0aYu0alq3r23-_PeSwmQKYZb-8LALxkVZSmTNjZ5oZ7qQJTe0lTJqZfDW5gnGwsnBM&usqp=CAU",
-        // })
+            image: "https://example.com/john.jpg",
+        });
 
         // Insert Labels
         const wipLabel = await Label.create({
             name: "wip",
             user: user._id,
+            color: "#ff9900",
             favourite: true,
-        })
-        const buyLabel = await Label.create({
-            name: "buy",
+        });
+
+        const homeLabel = await Label.create({
+            name: "Home",
             user: user._id,
-            favourite: true,
-        })
+            color: "#3366cc",
+        });
 
         // Insert Projects
-        const inboxProject = await Project.create({
-            name: "Inbox",
-            owner: user._id,
-            color: "#000000",
-            sections: [],
-            isInbox: true,
-        })
-
         const workProject = await Project.create({
-            name: "Work",
+            name: "Work Project",
             owner: user._id,
             color: "#11c023",
             sections: [],
             favourite: true,
-        })
+            isInbox: false,
+        });
+
+        const homeProject = await Project.create({
+            name: "Home Project",
+            owner: user._id,
+            color: "#ff6666",
+            sections: [],
+            favourite: true,
+            isInbox: false,
+        });
 
         // Insert Sections
-        const healthSection = await Section.create({
-            name: "Health",
-            project: inboxProject._id,
-        })
-
-        const financeSection = await Section.create({
-            name: "Finance",
-            project: inboxProject._id,
-        })
-
         const codingSection = await Section.create({
             name: "Coding",
             project: workProject._id,
-        })
-
-        const marketingSection = await Section.create({
-            name: "Marketing",
-            project: workProject._id,
-        })
-
-        // Update Project sections
-        inboxProject.sections.push(healthSection._id, financeSection._id)
-        await inboxProject.save()
-
-        workProject.sections.push(codingSection._id, marketingSection._id)
-        await workProject.save()
-
-        // Insert Task and Subtasks
-        const todoistCloneTask = await Task.create({
-            title: "Todoist Clone",
-            desc: "Clone the Todoist app",
-            project: workProject._id,
-            section: codingSection._id, // Assign to "Coding" section
-            subtasks: [],
-            user: user._id, // Assign the user
-            dueDate: new Date(),
-        })
-
-        codingSection.tasks.push(todoistCloneTask._id)
-        await codingSection.save()
-
-
-        const webSiteTask = await Task.create({
-            title: "Website",
-            desc: "Create website",
-            project: workProject._id,
-            section: marketingSection._id, // Assign to "Coding" section
-            subtasks: [],
-            user: user._id, // Assign the user
-            dueDate: new Date(),
-        })
-
-        marketingSection.tasks.push(webSiteTask._id)
-        await marketingSection.save()
-
-        const noSectionTask = await Task.create({
-            title: "No Section task",
-            desc: "no section task",
-            project: workProject._id,
-            subtasks: [],
-            user: user._id, // Assign the user
-            dueDate: new Date(),
-            labels: [buyLabel._id, wipLabel._id],
-        })
-        const noSectionTask2 = await Task.create({
-            title: "No Section 2",
-            desc: ".......",
-            project: workProject._id,
-            subtasks: [],
-            user: user._id, // Assign the user
-            dueDate: new Date(),
-        })
-        const healthTask = await Task.create({
-            title: "health tasks",
-            desc: ".......",
-            project: inboxProject._id,
-            subtasks: [],
-            user: user._id, // Assign the user
-            dueDate: new Date(),
-            section: healthSection._id,
-        })
-
-        healthSection.tasks.push(healthTask._id)
-        await healthSection.save()
-
-        const financeTask = await Task.create({
-            title: "finance tasks",
-            desc: ".......",
-            project: inboxProject._id,
-            subtasks: [],
-            user: user._id, // Assign the user
-            dueDate: new Date(),
-            section: financeSection._id,
-        })
-
-        financeSection.tasks.push(financeTask._id)
-        await financeSection.save()
-
-        const frontendSubtask = await Task.create({
-            title: "Frontend",
-            desc: "Develop frontend components",
-            project: workProject._id,
-            parent_task: todoistCloneTask._id, // Assign parent task
-            user: user._id, // Assign the user
-    
-        })
-
+            order: 1,
+            tasks: [], // Initialize tasks array
+        });
         
 
-        const backendSubtask = await Task.create({
-            title: "Backend",
-            desc: "Develop backend logic",
+        const shoppingSection = await Section.create({
+            name: "Shopping",
+            project: homeProject._id,
+            order: 1,
+            tasks: [], // Initialize tasks array
+        });
+
+        workProject.sections.push(codingSection._id)
+        homeProject.sections.push(shoppingSection._id)
+
+        await workProject.save()
+        await homeProject.save()
+
+        
+        
+        
+        
+        
+        // Insert Tasks
+        const codingTask = await Task.create({
+            title: "Complete Feature",
+            desc: "Finish implementing the new feature.",
+            completed: false,
+            dueDate: new Date("2023-09-10"),
+            subtasks: [],
+            comments: [],
+            priority: 3,
+            labels: [wipLabel._id],
             project: workProject._id,
             section: codingSection._id,
-            labels: [wipLabel._id], // Assign label "WIP"
-            parent_task: todoistCloneTask._id, // Assign parent task
-            user: user._id, // Assign the user
-            completed: true,
-            parentTask: todoistCloneTask._id,
-        })
+            user: user._id,
+            parentTask: null,
+            order: 1,
+        });
 
-        codingSection.tasks.push(backendSubtask._id)
-        await codingSection.save()
+        const shoppingTask = await Task.create({
+            title: "Grocery Shopping",
+            desc: "Buy groceries for the week.",
+            completed: false,
+            dueDate: new Date("2023-09-12"),
+            subtasks: [],
+            comments: [],
+            priority: 2,
+            labels: [homeLabel._id],
+            project: homeProject._id,
+            section: shoppingSection._id,
+            user: user._id,
+            parentTask: null,
+            order: 1,
+        });
+
+        // Update Section and Label tasks arrays
+        codingSection.tasks.push(codingTask._id);
+        await codingSection.save();
+
+        shoppingSection.tasks.push(shoppingTask._id);
+        await shoppingSection.save();
+
+        // Update Label tasks array
+        wipLabel.tasks.push(codingTask._id);
+        await wipLabel.save();
+
+        homeLabel.tasks.push(shoppingTask._id);
+        await homeLabel.save();
 
         const comment = await Comment.create({
             text: "This is amazing....",
             user: user._id,
-            task: todoistCloneTask._id, // Assign the comment to the Todoist Clone task
+            task: codingTask._id, // Assign the comment to the Todoist Clone task
             reactions: [
                 {
                     emoji: "👍",
@@ -216,7 +161,7 @@ async function insertSampleData() {
         const comment1 = await Comment.create({
             text: "great..",
             user: user._id,
-            task: todoistCloneTask._id, // Assign the comment to the Todoist Clone task
+            task: codingTask._id, // Assign the comment to the Todoist Clone task
             reactions: [
                 {
                     emoji: "⭐️",
@@ -229,28 +174,17 @@ async function insertSampleData() {
             ],
         })
 
-        todoistCloneTask.subtasks.push(frontendSubtask._id, backendSubtask._id)
-        todoistCloneTask.comments.push(comment._id, comment1._id)
+        codingTask.comments.push(comment._id, comment1._id)
+        await codingTask.save()
 
-        await todoistCloneTask.save()
 
-        wipLabel.tasks.push(backendSubtask)
-        await wipLabel.save()
 
-        buyLabel.tasks.push(noSectionTask)
-        await buyLabel.save()
 
-        console.log("Sample data inserted successfully.")
-
-        console.log(`inboxProject : ${inboxProject._id}`)
-        console.log(`workProject : ${workProject._id}`)
-
-        console.log(`${user.email} : ${user._id}`)
-        // console.log(`${user2.email} : ${user2._id}`)
+        console.log("Sample data inserted successfully.");
     } catch (error) {
-        console.error("Error inserting sample data:", error)
+        console.error("Error inserting sample data:", error);
     } finally {
-        mongoose.disconnect()
+        mongoose.disconnect();
     }
 }
 
@@ -259,3 +193,4 @@ deleteData().then(() => {
     // After deleting data, insert sample data
     insertSampleData()
 })
+
